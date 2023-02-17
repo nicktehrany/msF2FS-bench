dff738e27dced75e5aadb89f33a71e4a700bc79f
Nick Tehrany <nicktehrany1@gmail.com>
2023-02-17 17:09:14 +0100

    add f2fs figs

--- f2fs-baseline/plot.py
+++ f2fs-baseline/plot.py
@@ -41,15 +41,15 @@ def parse_fio_data(data_path, data):
     return 1
 
 def plot_throughput():
-    x = np.arange(0, 3)
+    x = np.arange(0, 5)
     width = 0.2
 
-    single_stream_iops = [None] * 3
-    single_stream_stddev = [None] * 3
-    two_stream_iops = [None] * 3
-    two_stream_stddev = [None] * 3
-    three_stream_iops = [None] * 3
-    three_stream_stddev = [None] * 3
+    single_stream_iops = [None] * 5
+    single_stream_stddev = [None] * 5
+    two_stream_iops = [None] * 5
+    two_stream_stddev = [None] * 5
+    three_stream_iops = [None] * 5
+    three_stream_stddev = [None] * 5
 
     for key, item in data.items():
         if 'single_stream-1' in key:
@@ -61,6 +61,12 @@ def plot_throughput():
         if 'single_stream-3' in key:
             single_stream_iops[2] = item['jobs'][0]['write']['iops']/1000
             single_stream_stddev[2] = item['jobs'][0]['write']['iops_stddev']/1000
+        if 'single_stream-4' in key:
+            single_stream_iops[3] = item['jobs'][0]['write']['iops']/1000
+            single_stream_stddev[3] = item['jobs'][0]['write']['iops_stddev']/1000
+        if 'single_stream-5' in key:
+            single_stream_iops[4] = item['jobs'][0]['write']['iops']/1000
+            single_stream_stddev[4] = item['jobs'][0]['write']['iops_stddev']/1000
         if 'two_stream-1' in key:
             two_stream_iops[0] = item['jobs'][0]['write']['iops']/1000
             two_stream_stddev[0] = item['jobs'][0]['write']['iops_stddev']/1000
@@ -70,6 +76,12 @@ def plot_throughput():
         if 'two_stream-3' in key:
             two_stream_iops[2] = item['jobs'][0]['write']['iops']/1000
             two_stream_stddev[2] = item['jobs'][0]['write']['iops_stddev']/1000
+        if 'two_stream-4' in key:
+            two_stream_iops[3] = item['jobs'][0]['write']['iops']/1000
+            two_stream_stddev[3] = item['jobs'][0]['write']['iops_stddev']/1000
+        if 'two_stream-5' in key:
+            two_stream_iops[4] = item['jobs'][0]['write']['iops']/1000
+            two_stream_stddev[4] = item['jobs'][0]['write']['iops_stddev']/1000
         if 'three_stream-1' in key:
             three_stream_iops[0] = item['jobs'][0]['write']['iops']/1000
             three_stream_stddev[0] = item['jobs'][0]['write']['iops_stddev']/1000
@@ -79,80 +91,46 @@ def plot_throughput():
         if 'three_stream-3' in key:
             three_stream_iops[2] = item['jobs'][0]['write']['iops']/1000
             three_stream_stddev[2] = item['jobs'][0]['write']['iops_stddev']/1000
+        if 'three_stream-4' in key:
+            three_stream_iops[3] = item['jobs'][0]['write']['iops']/1000
+            three_stream_stddev[3] = item['jobs'][0]['write']['iops_stddev']/1000
+        if 'three_stream-5' in key:
+            three_stream_iops[4] = item['jobs'][0]['write']['iops']/1000
+            three_stream_stddev[4] = item['jobs'][0]['write']['iops_stddev']/1000
 
     fig, ax = plt.subplots()
 
-    rects1 = ax.bar(x - width, single_stream_iops, yerr=single_stream_stddev, capsize=3, width=width, hatch='x', label="1 Stream")
-    rects2 = ax.bar(x, two_stream_iops, yerr=two_stream_stddev, capsize=3, width=width, hatch='', label="2 Streams")
-    rects3 = ax.bar(x + width, three_stream_iops, yerr=three_stream_stddev, capsize=3, width=width, hatch='/', label="3 Streams")
+    rects1 = ax.bar(x - width, single_stream_iops, yerr=single_stream_stddev, capsize=3, width=width, hatch='x', label="1 Concurrent Log")
+    rects2 = ax.bar(x, two_stream_iops, yerr=two_stream_stddev, capsize=3, width=width, hatch='', label="2 Concurrent Logs")
+    rects3 = ax.bar(x + width, three_stream_iops, yerr=three_stream_stddev, capsize=3, width=width, hatch='/', label="3 Concurrent Logs")
 
-    ax.bar_label(rects1, padding=3, fmt="%.1f")
-    ax.bar_label(rects2, padding=3, fmt="%.1f")
-    ax.bar_label(rects3, padding=3, fmt="%.1f")
-
-    # Plotting horizontal lines for max throughput on ZNS
-    plt.axhline(y = 257364.462305/1000, color = 'r', linestyle = ':', label = "ZNS 1 Zone")
-    plt.axhline(y = 459717.450472/1000, color = 'gray', linestyle = 'dashed', label = "ZNS 2 Zones")
-    plt.axhline(y = 378353.052926/1000, color = 'green', linestyle = 'dashdot', label = "ZNS 3 Zones")
-
-    fig.tight_layout()
-    # ax.grid(which='major', linestyle='dashed', linewidth='1')
-    ax.set_axisbelow(True)
-    ax.legend(loc='upper right', ncol=2)
-    ax.xaxis.set_ticks(x)
-    ax.xaxis.set_ticklabels(np.arange(1,4))
-    ax.set_ylim(bottom=0)
-    ax.set_ylabel('KIOPS')
-    ax.set_xlabel('Files per Stream')
-    plt.savefig(f'figs/f2fs_multi_stream_write.pdf', bbox_inches='tight')
-    plt.savefig(f'figs/f2fs_multi_stream_write.png', bbox_inches='tight')
-    plt.clf()
-
-# Only plot single file workloads
-def plot_throughput_first():
-    iops = [None] * 3
-    iops_stddev = [None] * 3
-
-    for key, item in data.items():
-        if 'single_stream-1' in key:
-            iops[0] = item['jobs'][0]['write']['iops']/1000
-            iops_stddev[0] = item['jobs'][0]['write']['iops_stddev']/1000
-        if 'two_stream-1' in key:
-            iops[1] = item['jobs'][0]['write']['iops']/1000
-            iops_stddev[1] = item['jobs'][0]['write']['iops_stddev']/1000
-        if 'three_stream-1' in key:
-            iops[2] = item['jobs'][0]['write']['iops']/1000
-            iops_stddev[2] = item['jobs'][0]['write']['iops_stddev']/1000
-
-    fig, ax = plt.subplots()
-
-    x = np.arange(0, 3)
-    rects1 = ax.bar(x, iops, yerr=iops_stddev, capsize=3, color='gray')
-
-    ax.bar_label(rects1, padding=3, fmt="%.1f")
-
-    # Plotting horizontal lines for max throughput on ZNS
-    plt.axhline(y = 257364.462305/1000, color = 'r', linestyle = ':', label = "ZNS 1 Zone")
-    plt.axhline(y = 459717.450472/1000, color = 'gray', linestyle = 'dashed', label = "ZNS 2 Zones")
-    plt.axhline(y = 378353.052926/1000, color = 'green', linestyle = 'dashdot', label = "ZNS 3 Zones")
+    # Somehow we have to force the hatch patterns
+    for i in range(len(single_stream_iops)):
+        rects1[i].set_edgecolor("black")
+        rects1[i].set_hatch("x")
+    for i in range(len(two_stream_iops)):
+        rects2[i].set_edgecolor("black")
+        rects2[i].set_hatch("o")
+    for i in range(len(three_stream_iops)):
+        rects3[i].set_edgecolor("black")
+        rects3[i].set_hatch("/")
 
     fig.tight_layout()
-    # ax.grid(which='major', linestyle='dashed', linewidth='1')
+    ax.grid(which='major', linestyle='dashed', linewidth='1')
     ax.set_axisbelow(True)
-    ax.legend(loc='upper right')
+    ax.legend(loc='best',ncol=2)
     ax.xaxis.set_ticks(x)
-    ax.xaxis.set_ticklabels(np.arange(1,4))
-    ax.set_ylim(bottom=0)
+    ax.xaxis.set_ticklabels(np.arange(1,6))
+    ax.set_ylim(bottom=0,top=350)
     ax.set_ylabel('KIOPS')
-    ax.set_xlabel('Streams')
-    plt.savefig(f'figs/f2fs_multi_type_write_1_file_per.pdf', bbox_inches='tight')
-    plt.savefig(f'figs/f2fs_multi_type_write_1_file_per.png', bbox_inches='tight')
+    ax.set_xlabel('Concurrent Files per Log')
+    plt.savefig(f'figs/f2fs-baseline.pdf', bbox_inches='tight')
+    plt.savefig(f'figs/f2fs-baseline.png', bbox_inches='tight')
     plt.clf()
 
 if __name__ == '__main__':
     file_path = '/'.join(os.path.abspath(__file__).split('/')[:-1])
 
-    parse_fio_data(f'{file_path}/f2fs_stream_data/', data)
+    parse_fio_data(f'{file_path}/data/', data)
 
     plot_throughput()
-    plot_throughput_first()
