25b6e2e9f9baa4e72c15eb355db4229dd03be36f
Nick Tehrany <nicktehrany1@gmail.com>
2023-02-20 12:19:29 +0000

    Add read latency bench setup

--- read-latency/plot.py
+++ read-latency/plot.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
-import math
 import numpy as np
 import matplotlib.patches as mpatches
 import os
 import glob
 import json
-import sys
-import getopt
 
 plt.rc('font', size=12)          # controls default text sizes
 plt.rc('axes', titlesize=12)     # fontsize of the axes title
@@ -17,8 +14,64 @@ plt.rc('xtick', labelsize=12)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=12)    # fontsize of the tick labels
 plt.rc('legend', fontsize=12)    # legend fontsize
 
-msf2fs_data = dict()
-f2fs_data = dict()
+msf2fs_iops_data = []
+f2fs_iops_data = []
+
+f2fs_global_data = dict()
+msf2fs_global_data = dict()
+
+msf2fs_lat_data = []
+f2fs_lat_data = []
+
+def parse_fio_log(data_path, data, type):
+    inited = False
+    shortest = 0
+
+    if not os.path.exists(f'{data_path}') or \
+            os.listdir(f'{data_path}') == []: 
+        print(f'No data in {data_path}')
+        return 0 
+
+    for file in glob.glob(f'{data_path}/*'): 
+        if "overwrite_iops_log" in file and type == "overwrite":
+            with open(file, 'r') as f:
+                iter = 0
+                for index, line in enumerate(f, 1):
+                    newline = line.split()
+                    if len(newline) < 1:
+                        break
+                    else:
+                        if not inited:
+                            data.append(int(newline[1][:-1]))
+                        else:
+                            if (len(data) > iter):
+                                data[iter] = data[iter] + int(newline[1][:-1])
+                        iter += 1
+                inited = True
+                if shortest == 0:
+                    shortest = iter
+                elif shortest > iter:
+                    shortest = iter
+
+        elif "overwrite_lat_log_clat" in file and type == "lat":
+            with open(file, 'r') as f:
+                iter = 0
+                for index, line in enumerate(f, 1):
+                    newline = line.split()
+                    if len(newline) < 1:
+                        break
+                    else:
+                        if not inited:
+                            data.append(int(newline[1][:-1]))
+                        else:
+                            data[iter] = data[iter] + int(newline[1][:-1])
+                inited = True
+                if shortest == 0:
+                    shortest = iter
+                elif shortest > iter:
+                    shortest = iter
+
+    return 1
 
 def parse_fio_data(data_path, data):
     if not os.path.exists(f'{data_path}') or \
@@ -26,7 +79,7 @@ def parse_fio_data(data_path, data):
         print(f'No data in {data_path}')
         return 0 
 
-    for file in glob.glob(f'{data_path}/*'): 
+    for file in glob.glob(f'{data_path}/write.json'): 
         with open(file, 'r') as f:
             for index, line in enumerate(f, 1):
                 # Removing all fio logs in json file by finding first {
@@ -43,125 +96,270 @@ def parse_fio_data(data_path, data):
 
     return 1
 
-def plot_throughput():
-    x = np.arange(0, 4)
-    width = 0.1
+def plot_iops():
+    f2fs_iops = [] 
+    f2fs_iops_x = []
+    msf2fs_iops = [] 
+    msf2fs_iops_x = []
 
-    msf2fs_seq_iops = [None] * 4
-    msf2fs_seq_stddev = [None] * 4
-    msf2fs_rand_iops = [None] * 4
-    msf2fs_rand_stddev = [None] * 4
-    f2fs_seq_iops = [None] * 4
-    f2fs_seq_stddev = [None] * 4
-    f2fs_rand_iops = [None] * 4
-    f2fs_rand_stddev = [None] * 4
+    total_bw = 0
+    bw_iter_tracker = 0
 
-    for key, item in msf2fs_data.items():
-        if '-1' in key:
-            for job in item['jobs']:
-                if 'seq' in job['jobname']:
-                    msf2fs_seq_iops[0] = job['read']['iops']/1000
-                    msf2fs_seq_stddev[0] = job['read']['iops_stddev']/1000
-                if 'rand' in job['jobname']:
-                    msf2fs_rand_iops[0] = job['read']['iops']/1000
-                    msf2fs_rand_stddev[0] = job['read']['iops_stddev']/1000
-        if '-10' in key:
-            for job in item['jobs']:
-                if 'seq' in job['jobname']:
-                    msf2fs_seq_iops[1] = job['read']['iops']/1000
-                    msf2fs_seq_stddev[1] = job['read']['iops_stddev']/1000
-                if 'rand' in job['jobname']:
-                    msf2fs_rand_iops[1] = job['read']['iops']/1000
-                    msf2fs_rand_stddev[1] = job['read']['iops_stddev']/1000
-        if '-50' in key:
-            for job in item['jobs']:
-                if 'seq' in job['jobname']:
-                    msf2fs_seq_iops[2] = job['read']['iops']/1000
-                    msf2fs_seq_stddev[2] = job['read']['iops_stddev']/1000
-                if 'rand' in job['jobname']:
-                    msf2fs_rand_iops[2] = job['read']['iops']/1000
-                    msf2fs_rand_stddev[2] = job['read']['iops_stddev']/1000
-        if '-100' in key:
-            for job in item['jobs']:
-                if 'seq' in job['jobname']:
-                    msf2fs_seq_iops[3] = job['read']['iops']/1000
-                    msf2fs_seq_stddev[3] = job['read']['iops_stddev']/1000
-                if 'rand' in job['jobname']:
-                    msf2fs_rand_iops[3] = job['read']['iops']/1000
-                    msf2fs_rand_stddev[3] = job['read']['iops_stddev']/1000
+    for iter in range(len(f2fs_iops_data)):
+        total_bw += f2fs_iops_data[iter]*4096
+        f2fs_iops.append(f2fs_iops_data[iter]/1000)
+        bw_iter_tracker += total_bw/1024/1024/1024/1024
+        f2fs_iops_x.append(bw_iter_tracker)
+        total_bw = 0
 
-    for key, item in f2fs_data.items():
-        if '-1' in key:
-            for job in item['jobs']:
-                if 'seq' in job['jobname']:
-                    f2fs_seq_iops[0] = job['read']['iops']/1000
-                    f2fs_seq_stddev[0] = job['read']['iops_stddev']/1000
-                if 'rand' in job['jobname']:
-                    f2fs_rand_iops[0] = job['read']['iops']/1000
-                    f2fs_rand_stddev[0] = job['read']['iops_stddev']/1000
-        if '-10' in key:
-            for job in item['jobs']:
-                if 'seq' in job['jobname']:
-                    f2fs_seq_iops[1] = job['read']['iops']/1000
-                    f2fs_seq_stddev[1] = job['read']['iops_stddev']/1000
-                if 'rand' in job['jobname']:
-                    f2fs_rand_iops[1] = job['read']['iops']/1000
-                    f2fs_rand_stddev[1] = job['read']['iops_stddev']/1000
-        if '-50' in key:
-            for job in item['jobs']:
-                if 'seq' in job['jobname']:
-                    f2fs_seq_iops[2] = job['read']['iops']/1000
-                    f2fs_seq_stddev[2] = job['read']['iops_stddev']/1000
-                if 'rand' in job['jobname']:
-                    f2fs_rand_iops[2] = job['read']['iops']/1000
-                    f2fs_rand_stddev[2] = job['read']['iops_stddev']/1000
-        if '-100' in key:
-            for job in item['jobs']:
-                if 'seq' in job['jobname']:
-                    f2fs_seq_iops[3] = job['read']['iops']/1000
-                    f2fs_seq_stddev[3] = job['read']['iops_stddev']/1000
-                if 'rand' in job['jobname']:
-                    f2fs_rand_iops[3] = job['read']['iops']/1000
-                    f2fs_rand_stddev[3] = job['read']['iops_stddev']/1000
+    total_bw = 0
+    bw_iter_tracker = 0
+
+    for iter in range(len(msf2fs_iops_data)):
+        total_bw += msf2fs_iops_data[iter]*4096
+        msf2fs_iops.append(msf2fs_iops_data[iter]/1000)
+        bw_iter_tracker += total_bw/1024/1024/1024/1024
+        msf2fs_iops_x.append(bw_iter_tracker)
+        total_bw = 0
+
+    # With 3 down-sampling
+    f2fs_iops_np = np.asarray(f2fs_iops)
+    f2fs_iops_fmt = np.nanmean(np.pad(f2fs_iops_np.astype(float), (0, 3 - f2fs_iops_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    f2fs_x_np = np.asarray(f2fs_iops_x)
+    f2fs_x_fmt = np.nanmean(np.pad(f2fs_x_np.astype(float), (0, 3 - f2fs_x_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    msf2fs_iops_np = np.asarray(msf2fs_iops)
+    msf2fs_iops_fmt = np.nanmean(np.pad(msf2fs_iops_np.astype(float), (0, 3 - msf2fs_iops_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    msf2fs_x_np = np.asarray(msf2fs_iops_x)
+    msf2fs_x_fmt = np.nanmean(np.pad(msf2fs_x_np.astype(float), (0, 3 - msf2fs_x_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+
+    print(f"F2FS Avg. KIOPS: {np.mean(f2fs_iops_np)}")
+    print(f"msF2FS  Avg. KIOPS: {np.mean(msf2fs_iops_np)}")
+
     fig, ax = plt.subplots()
+    
+    # With downsampling
+    ax.errorbar(f2fs_x_fmt, f2fs_iops_fmt, label="F2FS", fmt="-", linewidth=1)
+    ax.errorbar(msf2fs_x_fmt, msf2fs_iops_fmt, label="msF2FS", fmt="-", linewidth=1)
 
-    rects1 = ax.bar(x - (2 * width), f2fs_seq_iops, yerr=f2fs_seq_stddev, capsize=3, width=width, hatch='x', label="F2FS seqread")
-    rects2 = ax.bar(x - width, f2fs_rand_iops, yerr=f2fs_rand_stddev, capsize=3, width=width, hatch='/', label="F2FS randread")
-    rects3 = ax.bar(x + width, msf2fs_seq_iops, yerr=msf2fs_seq_stddev, capsize=3, width=width, hatch='x', label="msF2FS seqread")
-    rects4 = ax.bar(x + (2 * width), msf2fs_rand_iops, yerr=msf2fs_rand_iops, capsize=3, width=width, hatch='/', label="msF2FS randread")
+    # Without down-sampling
+    # ax.errorbar(f2fs_iops_x, f2fs_iops, label="F2FS", fmt="-", linewidth=0.5)
+    # ax.errorbar(msf2fs_iops_x, msf2fs_iops, label="msF2FS", fmt="-", linewidth=0.5)
+    
+    fig.tight_layout()
+    ax.grid(which='major', linestyle='dashed', linewidth='1')
+    ax.set_axisbelow(True)
+    ax.legend(loc='upper right')
+    ax.set_xlabel("Data Written (TiB)")
+    ax.set_ylabel("KIOPS")
+    ax.set_ylim(0, 300)
+    ax.set_xlim(0, 1.9)
+    plt.savefig(f'figs/gc-throughput.pdf', bbox_inches='tight')
+    plt.savefig(f'figs/gc-throughput.png', bbox_inches='tight')
+    plt.clf()
 
-    # For whatever reason we have to force the hatch patterns
-    for i in range(len(f2fs_seq_iops)):
-        rects1[i].set_edgecolor("black")
-        rects1[i].set_hatch("xx")
-    for i in range(len(f2fs_rand_iops)):
-        rects2[i].set_edgecolor("black")
-        rects2[i].set_hatch("/")
-    for i in range(len(msf2fs_seq_iops)):
-        rects3[i].set_edgecolor("black")
-        rects3[i].set_hatch("o")
-    for i in range(len(msf2fs_rand_iops)):
-        rects4[i].set_edgecolor("black")
-        rects4[i].set_hatch("\\")
+def plot_tail_latency():
+    
+    f2fs_lats = []
+    msf2fs_lats = []
+
+    for key, item in f2fs_global_data.items():
+        # f2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['95.000000'])/1000)
+        f2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['99.000000'])/1000)
+        f2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['99.900000'])/1000)
+        f2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['99.990000'])/1000)
+        f2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['99.999000'])/1000)
+        f2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['100.000000'])/1000)
+
+    for key, item in msf2fs_global_data.items():
+        # msf2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['95.000000'])/1000)
+        msf2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['99.000000'])/1000)
+        msf2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['99.900000'])/1000)
+        msf2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['99.990000'])/1000)
+        msf2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['99.999000'])/1000)
+        msf2fs_lats.append(int(item['jobs'][0]['write']['clat_ns']['percentile']['100.000000'])/1000)
+
+    fig, ax = plt.subplots()
+    
+    ax.errorbar([1,2,3,4,5], f2fs_lats, label="F2FS", fmt="-", linewidth=1)
+    ax.errorbar([1,2,3,4,5], msf2fs_lats, label="msF2FS", fmt="-", linewidth=1)
 
     fig.tight_layout()
     ax.grid(which='major', linestyle='dashed', linewidth='1')
     ax.set_axisbelow(True)
-    ax.legend(loc='best',ncol=2)
-    ax.xaxis.set_ticks(x)
-    ax.xaxis.set_ticklabels([1,10,50,100])
-    ax.set_ylim(bottom=0,top=600)
-    ax.set_ylabel('KIOPS')
-    ax.set_xlabel('Concurrent Files')
-    plt.savefig(f'figs/msf2fs-read-throughput.pdf', bbox_inches='tight')
-    plt.savefig(f'figs/msf2fs-read-throughput.png', bbox_inches='tight')
+    plt.yscale("log")
+    ax.xaxis.set_ticks([1,2,3,4,5])
+    ax.xaxis.set_ticklabels(["99", "99.9", "99.99", "99.999", "100"])
+    ax.legend(loc='best')
+    ax.set_xlabel("Percentile")
+    ax.set_ylabel("Latency (usec)")
+    # ax.set_ylim(1, 100000)
+    plt.savefig(f'figs/gc-tail_lat.pdf', bbox_inches='tight')
+    plt.savefig(f'figs/gc-tail_lat.png', bbox_inches='tight')
+    plt.clf()
+
+def plot_latency_f2fs():
+    f2fs_lat = [] 
+    f2fs_lat_x = []
+    msf2fs_lat = [] 
+    msf2fs_lat_x = []
+
+    total_bw = 0
+    bw_iter_tracker = 0
+
+    min = 0
+    if len(f2fs_lat_data) > len(f2fs_iops_data):
+        min = len(f2fs_iops_data) - 1
+    else:
+        min = len(f2fs_lat_data) - 1
+    
+    for iter in range(min):
+        total_bw += f2fs_iops_data[iter]*4096
+        f2fs_lat.append(f2fs_lat_data[iter]/1000)
+        bw_iter_tracker += total_bw/1024/1024/1024/1024
+        f2fs_lat_x.append(bw_iter_tracker)
+        total_bw = 0
+
+    total_bw = 0
+    bw_iter_tracker = 0
+
+    min = 0
+    if len(msf2fs_lat_data) > len(msf2fs_iops_data):
+        min = len(msf2fs_iops_data) - 1
+    else:
+        min = len(msf2fs_lat_data) - 1
+
+    for iter in range(min):
+        total_bw += msf2fs_iops_data[iter]*4096
+        msf2fs_lat.append(msf2fs_lat_data[iter]/1000)
+        bw_iter_tracker += total_bw/1024/1024/1024/1024
+        msf2fs_lat_x.append(bw_iter_tracker)
+        total_bw = 0
+
+    # With 3 down-sampling
+    f2fs_lat_np = np.asarray(f2fs_lat)
+    f2fs_lat_fmt = np.nanmean(np.pad(f2fs_lat_np.astype(float), (0, 3 - f2fs_lat_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    f2fs_x_np = np.asarray(f2fs_lat_x)
+    f2fs_x_fmt = np.nanmean(np.pad(f2fs_x_np.astype(float), (0, 3 - f2fs_x_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    msf2fs_lat_np = np.asarray(msf2fs_lat)
+    msf2fs_lat_fmt = np.nanmean(np.pad(msf2fs_lat_np.astype(float), (0, 3 - msf2fs_lat_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    msf2fs_x_np = np.asarray(msf2fs_lat_x)
+    msf2fs_x_fmt = np.nanmean(np.pad(msf2fs_x_np.astype(float), (0, 3 - msf2fs_x_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+
+    print(f"F2FS Avg. lat: {np.mean(f2fs_lat_np)}")
+
+    fig, ax = plt.subplots()
+    
+    # With downsampling
+    # ax.errorbar(f2fs_x_fmt, f2fs_lat_fmt, label="F2FS", fmt="-", linewidth=1)
+    # ax.errorbar(msf2fs_x_fmt, msf2fs_lat_fmt, label="msF2FS", fmt="-", linewidth=1)
+
+    # Without down-sampling
+    ax.errorbar(f2fs_lat_x, f2fs_lat, label="F2FS", fmt="-", linewidth=0.5)
+    # ax.errorbar(msf2fs_lat_x, msf2fs_lat, label="msF2FS", fmt="-", linewidth=0.5)
+    
+    fig.tight_layout()
+    ax.grid(which='major', linestyle='dashed', linewidth='1')
+    ax.set_axisbelow(True)
+    ax.legend(loc='upper right')
+    ax.set_xlabel("Data Written (TiB)")
+    ax.set_ylabel("Average Latency (usec)")
+    ax.set_ylim(0, 1000)
+    ax.set_xlim(0, 1.9)
+    plt.savefig(f'figs/gc-latency-f2fs.pdf', bbox_inches='tight')
+    plt.savefig(f'figs/gc-latency-f2fs.png', bbox_inches='tight')
+    plt.clf()
+
+def plot_latency_msf2fs():
+    f2fs_lat = [] 
+    f2fs_lat_x = []
+    msf2fs_lat = [] 
+    msf2fs_lat_x = []
+
+    total_bw = 0
+    bw_iter_tracker = 0
+
+    min = 0
+    if len(f2fs_lat_data) > len(f2fs_iops_data):
+        min = len(f2fs_iops_data) - 1
+    else:
+        min = len(f2fs_lat_data) - 1
+    
+    for iter in range(min):
+        total_bw += f2fs_iops_data[iter]*4096
+        f2fs_lat.append(f2fs_lat_data[iter]/1000)
+        bw_iter_tracker += total_bw/1024/1024/1024/1024
+        f2fs_lat_x.append(bw_iter_tracker)
+        total_bw = 0
+
+    total_bw = 0
+    bw_iter_tracker = 0
+
+    min = 0
+    if len(msf2fs_lat_data) > len(msf2fs_iops_data):
+        min = len(msf2fs_iops_data) - 1
+    else:
+        min = len(msf2fs_lat_data) - 1
+
+    for iter in range(min):
+        total_bw += msf2fs_iops_data[iter]*4096
+        msf2fs_lat.append(msf2fs_lat_data[iter]/1000)
+        bw_iter_tracker += total_bw/1024/1024/1024/1024
+        msf2fs_lat_x.append(bw_iter_tracker)
+        total_bw = 0
+
+    # With 3 down-sampling
+    f2fs_lat_np = np.asarray(f2fs_lat)
+    f2fs_lat_fmt = np.nanmean(np.pad(f2fs_lat_np.astype(float), (0, 3 - f2fs_lat_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    f2fs_x_np = np.asarray(f2fs_lat_x)
+    f2fs_x_fmt = np.nanmean(np.pad(f2fs_x_np.astype(float), (0, 3 - f2fs_x_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    msf2fs_lat_np = np.asarray(msf2fs_lat)
+    msf2fs_lat_fmt = np.nanmean(np.pad(msf2fs_lat_np.astype(float), (0, 3 - msf2fs_lat_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+    msf2fs_x_np = np.asarray(msf2fs_lat_x)
+    msf2fs_x_fmt = np.nanmean(np.pad(msf2fs_x_np.astype(float), (0, 3 - msf2fs_x_np.size%3), mode='constant', constant_values=np.NaN).reshape(-1, 3), axis=1)
+
+    print(f"msF2FS  Avg. lat: {np.mean(msf2fs_lat_np)}")
+
+    fig, ax = plt.subplots()
+    
+    # With downsampling
+    # ax.errorbar(f2fs_x_fmt, f2fs_lat_fmt, label="F2FS", fmt="-", linewidth=1)
+    ax.errorbar(msf2fs_x_fmt, msf2fs_lat_fmt, label="msF2FS", fmt="-", linewidth=1)
+
+    # Without down-sampling
+    # ax.errorbar(f2fs_lat_x, f2fs_lat, label="F2FS", fmt="-", linewidth=0.5)
+    # ax.errorbar(msf2fs_lat_x, msf2fs_lat, label="msF2FS", fmt="-", linewidth=0.5)
+    
+    fig.tight_layout()
+    ax.grid(which='major', linestyle='dashed', linewidth='1')
+    ax.set_axisbelow(True)
+    ax.legend(loc='upper right')
+    ax.set_xlabel("Data Written (TiB)")
+    ax.set_ylabel("Average Latency (usec)")
+    ax.set_ylim(0, 1000)
+    ax.set_xlim(0, 1.9)
+    plt.savefig(f'figs/gc-latency-msf2fs.pdf', bbox_inches='tight')
+    plt.savefig(f'figs/gc-latency-msf2fs.png', bbox_inches='tight')
     plt.clf()
 
 if __name__ == '__main__':
     file_path = '/'.join(os.path.abspath(__file__).split('/')[:-1])
 
-    parse_fio_data(f'{file_path}/data-spf/', msf2fs_data)
-    parse_fio_data(f'{file_path}/data-f2fs/', f2fs_data)
+    parse_fio_log(f'{file_path}/data-f2fs/', f2fs_iops_data, "overwrite")
+    parse_fio_log(f'{file_path}/data-spf/', msf2fs_iops_data, "overwrite")
 
-    plot_throughput()
-    # TODO plot tail latency for one of them probably 100 files as line (equal distance and points for 95, 99, 99.9, 99.99)
+    # throughput over time plot
+    plot_iops()
+
+    parse_fio_data(f'{file_path}/data-f2fs/', f2fs_global_data)
+    parse_fio_data(f'{file_path}/data-spf/', msf2fs_global_data)
+
+    # # tail latency plot
+    plot_tail_latency()
+
+    parse_fio_log(f'{file_path}/data-f2fs/', f2fs_lat_data, "lat")
+    parse_fio_log(f'{file_path}/data-spf/', msf2fs_lat_data, "lat")
+
+    # latency over time plot
+    plot_latency_f2fs()
+    plot_latency_msf2fs()
+
