d550cf5791e20576e99491db863a18150ec16d66
Nick Tehrany <nicktehrany1@gmail.com>
2023-03-04 19:56:41 +0100

    Add rocksdb figs

--- zbdbench-data/plot.py
+++ zbdbench-data/plot.py
@@ -15,8 +15,8 @@ plt.rc('xtick', labelsize=12)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=12)    # fontsize of the tick labels
 plt.rc('legend', fontsize=12)    # legend fontsize
 
-def plot_throughput(f2fs_iops, msf2fs_spf_iops, msf2fs_spf_e_iops, msf2fs_amfs_iops, zenfs_iops, x_labs, name):
-    x = np.arange(0, 2)
+def plot_throughput(f2fs_iops, msf2fs_spf_iops, msf2fs_spf_e_iops, msf2fs_amfs_iops, zenfs_iops, x_labs, name, max, lab):
+    x = np.arange(0, len(f2fs_iops))
 
     fig, ax = plt.subplots()
 
@@ -46,17 +46,17 @@ def plot_throughput(f2fs_iops, msf2fs_spf_iops, msf2fs_spf_e_iops, msf2fs_amfs_i
     fig.tight_layout()
     ax.set_axisbelow(True)
     ax.grid(which='major', linestyle='dashed', linewidth='1')
-    ax.legend(loc='upper right', ncol=2)
+    ax.legend(loc='best', ncol=2)
     ax.xaxis.set_ticks(x)
     ax.xaxis.set_ticklabels(x_labs)
-    ax.set_ylim(bottom=0,top=150)
-    ax.set_ylabel('1000 Ops/sec')
+    ax.set_ylim(bottom=0,top=max)
+    ax.set_ylabel(lab)
     plt.savefig(f'figs/rocksdb-throughput-{name}.pdf', bbox_inches='tight')
     plt.savefig(f'figs/rocksdb-throughput-{name}.png', bbox_inches='tight')
     plt.clf()
 
-def plot_latency(f2fs_iops, msf2fs_spf_iops, msf2fs_spf_e_iops, msf2fs_amfs_iops, zenfs_iops, x_labs, name):
-    x = np.arange(0, 2)
+def plot_latency(f2fs_iops, msf2fs_spf_iops, msf2fs_spf_e_iops, msf2fs_amfs_iops, zenfs_iops, x_labs, name, max):
+    x = np.arange(0, len(f2fs_iops))
 
     fig, ax = plt.subplots()
 
@@ -86,60 +86,77 @@ def plot_latency(f2fs_iops, msf2fs_spf_iops, msf2fs_spf_e_iops, msf2fs_amfs_iops
     fig.tight_layout()
     ax.set_axisbelow(True)
     ax.grid(which='major', linestyle='dashed', linewidth='1')
-    ax.legend(loc='upper right', ncol=2)
+    ax.legend(loc='best', ncol=2)
     ax.xaxis.set_ticks(x)
     ax.xaxis.set_ticklabels(x_labs)
-    ax.set_ylim(bottom=0,top=150)
-    ax.set_ylabel('Latency (msec)')
+    ax.set_ylim(bottom=0,top=max)
+    ax.set_ylabel('Latency (usec)')
     plt.savefig(f'figs/rocksdb-latency-{name}.pdf', bbox_inches='tight')
     plt.savefig(f'figs/rocksdb-latency-{name}.png', bbox_inches='tight')
     plt.clf()
+
 if __name__ == '__main__':
     file_path = '/'.join(os.path.abspath(__file__).split('/')[:-1])
 
     # Write data
-    f2fs_write = [73.226, 47.653]
-    msf2fs_spf_write = [70.887, 64.156]
+    f2fs_write = [73.795, 51.429]
+    msf2fs_spf_write = [70.616, 60.922]
     msf2fs_spf_e_write = [0, 0]
     msf2fs_amfs_write = [0 , 0]
     zenfs_write = [0, 0]
 
-    plot_throughput(f2fs_write, msf2fs_spf_write, msf2fs_spf_e_write, msf2fs_amfs_write, zenfs_write, ["fillrandom", "overwrite"], "write")
+    plot_throughput(f2fs_write, msf2fs_spf_write, msf2fs_spf_e_write, msf2fs_amfs_write, zenfs_write, ["fillrandom", "overwrite"], "write", 130, "1000 Ops/sec")
 
     # Read data
-    f2fs_read = [0.211, 0.202]
-    msf2fs_spf_read = [0.094, 0.116]
-    msf2fs_spf_e_read = [0, 0]
-    msf2fs_amfs_read = [0 , 0]
-    zenfs_read = [0, 0]
+    f2fs_read = [623, 610, 616]
+    msf2fs_spf_read = [1222, 998, 959]
+    msf2fs_spf_e_read = [0, 0, 0]
+    msf2fs_amfs_read = [0 , 0, 0]
+    zenfs_read = [0, 0, 0]
 
-    plot_throughput(f2fs_read, msf2fs_spf_read, msf2fs_spf_e_read, msf2fs_amfs_read, zenfs_read, ["randread", "readwhilewrite"], "read")
-
-    # Write and read data for write limited 20% readwhilewriting
-    # f2fs_rw = []
-    # msf2fs_spf_rw = []
-    # msf2fs_spf_e_rw = []
-    # msf2fs_amfs_rw = []
-    # zenfs_rw = []
-
-    # plot_throughput(f2fs_rw, msf2fs_spf_rw, msf2fs_spf_e_rw, msf2fs_amfs_rw, zenfs_rw, ["read", "write"])
+    plot_throughput(f2fs_read, msf2fs_spf_read, msf2fs_spf_e_read, msf2fs_amfs_read, zenfs_read, ["randread", "readwhilewrite", "readwhilewriting (20MiB)"], "read", 1500, "Ops/sec")
 
     # Write Lats
-    f2fs_lats = [0, 0]
-    msf2fs_spf_lats = [0, 0]
+    f2fs_lats = [14.738049, 25.977038]
+    msf2fs_spf_lats = [16.053960, 29.752540]
     msf2fs_spf_e_lats = [0, 0]
     msf2fs_amfs_lats = [0, 0]
     zenfs_lats = [0, 0]
 
-    plot_latency(f2fs_lats, msf2fs_spf_lats, msf2fs_spf_e_lats, msf2fs_amfs_lats, zenfs_lats, ["99", "99.99"], "write")
+    plot_latency(f2fs_lats, msf2fs_spf_lats, msf2fs_spf_e_lats, msf2fs_amfs_lats, zenfs_lats, ["P95", "P99"], "write", 50)
+
+    # overwrite Lats
+    f2fs_lats = [17.586594, 35.767433]
+    msf2fs_spf_lats = [18.232131, 32.566096]
+    msf2fs_spf_e_lats = [0, 0]
+    msf2fs_amfs_lats = [0, 0]
+    zenfs_lats = [0, 0]
+
+    plot_latency(f2fs_lats, msf2fs_spf_lats, msf2fs_spf_e_lats, msf2fs_amfs_lats, zenfs_lats, ["P95", "P99"], "overwrite", 50)
 
     # Read Lats
-    f2fs_lats = [0, 0]
-    msf2fs_spf_lats = [0, 0]
+    f2fs_lats = [416.587603, 2411.761345]
+    msf2fs_spf_lats = [417.537183, 12845.643518]
     msf2fs_spf_e_lats = [0, 0]
     msf2fs_amfs_lats = [0, 0]
     zenfs_lats = [0, 0]
 
-    plot_latency(f2fs_lats, msf2fs_spf_lats, msf2fs_spf_e_lats, msf2fs_amfs_lats, zenfs_lats, ["99", "99.99"], "read")
+    plot_latency(f2fs_lats, msf2fs_spf_lats, msf2fs_spf_e_lats, msf2fs_amfs_lats, zenfs_lats, ["P95", "P99"], "read", 20000)
 
-    # possible readwhilewriting lats
+    # readwhilewriting Lats
+    f2fs_lats = [487.962679, 3344.948109]
+    msf2fs_spf_lats = [284.704503, 1141.125387]
+    msf2fs_spf_e_lats = [0, 0]
+    msf2fs_amfs_lats = [0, 0]
+    zenfs_lats = [0, 0]
+
+    plot_latency(f2fs_lats, msf2fs_spf_lats, msf2fs_spf_e_lats, msf2fs_amfs_lats, zenfs_lats, ["P95", "P99"], "readwhilewriting", 5000)
+
+    # readwhilewriting writelimited Lats
+    f2fs_lats = [600.258019, 22344.327214]
+    msf2fs_spf_lats = [328.609658, 6111.381415]
+    msf2fs_spf_e_lats = [0, 0]
+    msf2fs_amfs_lats = [0, 0]
+    zenfs_lats = [0, 0]
+
+    plot_latency(f2fs_lats, msf2fs_spf_lats, msf2fs_spf_e_lats, msf2fs_amfs_lats, zenfs_lats, ["P95", "P99"], "readwhilewriting-limited", 30000)
